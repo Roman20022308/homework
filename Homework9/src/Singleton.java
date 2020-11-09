ec0edc9d198bae4ac1748e6eb48ff21d1bbe55f2
Yusufyanov.roma <Yusufyanov.roma@mail.ru>
2020-11-09 23:07:14 +0300

    ns

--- Homework9/src/Singleton.java
+++ Homework9/src/Singleton.java
@@ -1,14 +1,24 @@
 public final class Singleton {
     private static Singleton instance;
-    String value="s";
+    String value;
+
     private Singleton(String value) {
+
+        // Этот код эмулирует медленную инициализацию
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException ex) {
+            ex.printStackTrace();
+        }
         this.value = value;
+
     }
 
     static Singleton getInstance(String value)
     {
         if (instance == null) {
-            instance = new Singleton(value);
+            synchronized(Singleton.class){
+            instance = new Singleton(value);}
         }
         return instance;
     }
